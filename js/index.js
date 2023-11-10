3c2413a0f5bfea9a7b50209fbaa44f415671d316
Nicolas Lagos <niicolagos@hotmail.com>
2023-11-10 09:40:39 -0300

    se agregan cuotas al presupuesto

--- js/index.js
+++ js/index.js
@@ -1,22 +1,23 @@
+// Modifica la función calculateBudget en script.js
 function calculateBudget() {
-    // Obtener los valores de ingresos y gastos
-    const ingreso = parseFloat(document.getElementById('ingreso').value);
-    const expensas = parseFloat(document.getElementById('expensas').value);
-
-    // Calcular el presupuesto restante
-    let budget = ingreso - expensas;
-
-    // Determinar el mensaje del resultado
-    let resultMessage = '';
-    if (budget > 0) {
-        resultMessage = '¡Felicidades! Tienes un excedente de $' + budget.toFixed(2) + ' al mes.';
-    } else if (budget === 0) {
-        resultMessage = 'Estás equilibrado. No tienes excedente ni deuda.';
-    } else {
-        resultMessage = '¡Cuidado! Tienes un déficit de $' + Math.abs(budget).toFixed(2) + ' al mes.';
+    // Obtener los valores de precio y cuotas
+    const price = parseFloat(document.getElementById('price').value);
+    const installments = parseInt(document.getElementById('installments').value);
+  
+    // Validar que la cantidad de cuotas sea mayor que cero
+    if (installments <= 0) {
+      alert('Por favor, ingrese una cantidad válida de cuotas.');
+      return;
     }
-
+  
+    // Calcular el monto de cada cuota
+    let installmentAmount = price / installments;
+  
     // Mostrar el resultado
-    document.getElementById('result').innerText = resultMessage;
+    let resultMessage = 'Total a Pagar: $' + price.toFixed(2) + '<br>' +
+                        'Cuotas: ' + installments + '<br>' +
+                        'Monto de cada Cuota: $' + installmentAmount.toFixed(2);
+  
+    document.getElementById('result').innerHTML = resultMessage;
     document.getElementById('result-container').style.display = 'block';
-}
+  }
