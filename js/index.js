32df6dbe77bc9bba6e61d7d8fe09828aceaa1eff
Nicolas Lagos <niicolagos@hotmail.com>
2023-11-27 18:04:22 -0300

    se agregan comentarios

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-//Función CalculateLoan
+//Función Calculadora de prestamos
 function CalculateLoan() {
-    // Obtener los valores de precio y cuotas
+    // Obtener los valores de precio, intereses y cuotas
     let amount = parseFloat(document.getElementById('amount').value);
     let interest = parseFloat(document.getElementById('interest').value);
     let deadlines = parseInt(document.getElementById('deadlines').value);
@@ -8,22 +8,22 @@ function CalculateLoan() {
     //Validar que los valores ingresados sean numericos
 
     if (isNaN(amount) || isNaN(interest) || isNaN(deadlines)){
-        ShowError("Por favor, ingrese valores numericos validos");
+        alert("Por favor, ingrese valores numericos validos");
         return;
     }
 
     if (amount <= 0){
-        ShowError("El monto del prestamo debe ser mayor que cero");
+        alert("El monto del prestamo debe ser mayor que cero");
         return;
     }
 
     if (interest <= 0 || interest > 100){
-        ShowError("La tasa de interes debe estar entre 0 y 100");
+        alert("La tasa de interes debe estar entre 0 y 100");
         return;
     }
 
     if (deadlines <= 0 || deadlines > 360){
-        ShowError("El plazo debe estar entre 1 y 360 meses");
+        alert("El plazo debe estar entre 1 y 360 meses");
         return;
     }
 
@@ -36,7 +36,7 @@ function CalculateLoan() {
     ResultElement.innerHTML = `El pago mensual es : $${MonthlyPayment.toFixed(2)}`;
 }
 
-    function ShowError(message){
+    function alert(message){
         const errorElement = document.getElementById("error");
         errorElement.style.display = "block"; //mostrar el elemento de error
         //Establecer el mensaje de error recibido como parametro
